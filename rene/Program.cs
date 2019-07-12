77dfb93c294fe4be421ea8aa7d3190e77d48ef07
Ids Ids <ids@unicaescri.onmicrosoft.com>
2019-07-12 10:35:00 -0600

    hello

---
+++ rene/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace rene
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("rene");
+            Console.ReadLine();
+
+        }
+    }
+}
